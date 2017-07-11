f7584ad71ffc8ef7039ff52047f1b9d20f570402
eruber <eruber@gmail.com>
2017-07-11 09:07:58 -0700

    Working prior to changing interface to support cfg strings for round-trip via YAML support

--- config.py
+++ config.py
@@ -1,18 +1,9 @@
 #!/usr/bin/env python
 #coding=utf-8
 """
-config
+Module config
 
-Abstract Base Class for implementing a simple configuration manager.
-Sub-class it and provide your own read/write methods.
-
-If you really want a persistent data store module rather than a config module,
-chech out: 
-            shelve - Python object persistence
-
-you can find it in the Python Standard Library on-line documentation:
-
-    https://docs.python.org/3/library/shelve.html
+.. moduleauthor:: E.R. Uber <eruber@gmail.com>
 
 """
 import os.path
@@ -21,44 +12,101 @@ import abc
 #------------------------------------------------------------------------------
 class ConfigFileNamesDirException(Exception):
     """
-    Custom exeception
+    Custom exeception covering the logical case that the **cfgfile** specified is
+    indeed not a file, but a directory. I know, who would do such a thing?
     """
     pass
 
 #------------------------------------------------------------------------------
 
-
 #------------------------------------------------------------------------------
 class Config(metaclass=abc.ABCMeta):
+    """
+    Abstract Base Class for implementing a simple configuration manager.
+    Sub-class it and provide your own read/write methods. 
 
+    Args:
+
+        **cfgfile** - a string 
+
+        Specifies the configuration file name, relative or absolute.
+        The default is the value of the **DEFAULT_CFG_FILE** attribute.
+
+        **encoding** - a string 
+
+        Specifies the string encoding for the configuration filem, **cfgfile**.
+        Use this parameter to change the unicode encoding used to read/write the **cfgfile**.
+        The default is the value of the **DEFAULT_ENCODING** attribute.
+    
+        **cfg**  - a dictionary
+
+        Specifies the configuration state used to initialize the configuration file, **cfgfile**, if it does not
+        already exist; if it does exist, this dictionary will be over-written by the contents of the
+        configuration file, **cfgfile**. To force re-iniitalization of the **cfgfile** with this dictionary
+        after the file already exists, use the **force** parameter. The default is the value of the
+        DEFAULT_CFG attribute.
+    
+        **force**   - a boolean
+
+        If True, forces the contents of the configuration dictionary specified, **cfg**, to over-write any
+        existing **cfgfile**. The default behavior is for the configuration file to be read and used to initialize 
+        the configuration dictionary, **cfg**. The default is the value of the **DEFAULT_FORCE** attribute.
+
+        **write_thru** - a boolean
+
+        If True, any changes made to the **cfg** property via an assignment will result in the configuration file, 
+        **cfgfile**, being immediately updated. If False, the configurate dictionary and the configuration file will not
+        be in sync until the write method is executed. The default is the value of the **DEFAULT_WRITE_THRU** attribute.
+
+    .. note:: If any of the class constructor parameters passed are not of the correct type,
+              the default value for that parameter will be used. See Class Attributes for
+              default values.
+
+    Returns:
+
+        An object instatiation of the Confg Manager class, Config, as defined herein.
+
+    Raises:
+
+        TypeError
+
+    .. warning:: Instantiating this class directly will result in a TypeError exception being raised. 
+
+    See test_config.py in the tests directory which includes this test.
+
+    Class Attributes:
+
+    .. note:: Class Attributes are not an attribute of an *instance* of a class (ie, the object); they are an attribute of the class itself.
+
+    .. note:: All Class Attributes can be over-ridden by the sub-class.
+
+    """
+
+    #--------------------------------------------------------------------------
     # Class Attributes, 
     # not an attribute of an *instance* of a class, but the class itself
+    #--------------------------------------------------------------------------
+
+
+    #: Default configuration file name, cfgfile, if none is specified during class instantiation.
     DEFAULT_CFG_FILE   = "config.cfg"
+
+    #: Default configuration dictionary, cfg, if none is specified during class instantiation.
     DEFAULT_CFG        = {}
+
+    #: Default force parameter value, force, if none is specified during class instantiation.
     DEFAULT_FORCE      = False
+
+    #: Default write_thru parameater value, write_thru, if none is specified during class instantiation.
     DEFAULT_WRITE_THRU = False
+
+    #: Default configuration file text encoding, encoding, if none is specified during class instantiation.
     DEFAULT_ENCODING   = 'utf-8'
 
+
     def __init__(self, cfgfile=None, encoding=None, cfg=None, force=None, write_thru=None):
-        """
-        cfgfile - configurate file name, relative or absolute
 
-        encoding - default str encoding for Python 3 is utf-8, use this parameter
-                   to change the unicode encoding used to read/write the cfgfile 
-        
-        cfg     - configuration dictionary, defaults to empty
-        
-        force   - boolean, if True, forces Config object initialization 
-                  to init the config to the default cfg rather than read
-                  it from the file system it the cfg exits in the file system
-
-        write_thru - boolean, if True, any changes made in memory to cfg
-                     by using the setter property will be immediated written 
-                     thru to the file system
-        """
-        cfg_file = cfgfile if isinstance(cfgfile, str) else self.DEFAULT_CFG_FILE
-
-        self._cfgfile    = os.path.abspath(cfg_file)
+        self._cfgfile    = os.path.abspath(cfgfile if isinstance(cfgfile, str) else self.DEFAULT_CFG_FILE)
         self._encoding   = encoding if isinstance(encoding, str) else self.DEFAULT_ENCODING
         self._cfg        = cfg if isinstance(cfg, dict) else self.DEFAULT_CFG
         self._force      = force if isinstance(force, bool) else self.DEFAULT_FORCE
@@ -101,23 +149,46 @@ class Config(metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def read(self):
         """
-        Reads the cfgfile, returns cfg if not errors.
-        This method should be over-ridden in a sub-class.
+        Reads the **cfgfile**.
+
+        Returns: 
+
+            The configuration dictionary accessible by the **cfg** property.
+
+        .. note:: This method should be over-ridden by a sub-class.
+        
         """
-        #raise NotImplementedError
 
     @abc.abstractmethod
     def write(self):
         """
-        Writes cfg to file system and return it.
-        This method should be over-ridden in a sub-class.
+        Writes **cfg** to file system using the file name **cfgfile**.
+
+        Returns:
+
+            None
+
+        .. note:: This method should be over-ridden by a sub-class.
+
         """
-        #raise NotImplementedError
 
     @property
     def cfg(self):
         """
-        Returns the instance of the configuration dictionary
+        Property
+
+        **cfg** - the configuration dictionary
+
+        Returns the configuration dictionary.
+
+        Can be used to set the configuration dictionary.
+        If set and the **writethru** property is True, then
+        the dictionary will be immdediately written to the file system using the filename **cfgfile**.
+
+        Raises:
+
+            TypeError if **cfg** assignment value is not a dictionary.
+
         """
         return(self._cfg)
 
@@ -133,12 +204,26 @@ class Config(metaclass=abc.ABCMeta):
             self._cfg = dict_value
             if self._write_thru:
                 self.write()
+        else:
+            raise TypeError("Assignment value to cfg vmust be a dictionary!")
 
 
     @property
     def writethru(self):
         """
-        Returns the boolean value of the write thru property
+        Property
+
+        **writethru** - a boolean
+
+        If set to True, any dictionary assignments to the **cfg** property will result in the **cfgfile** being updated.
+
+        If set to False, any dictionary assignments to the **cfg** property will be to the dictionary in memory only, 
+        not to the **cfgfile**. An explicit **write()** will have to be done by the user to update **cfgfile**.
+
+        Raises:
+
+            TypeError if **writethru** assignment value is not a boolean.
+
         """
         return(self._write_thru)
 
@@ -149,11 +234,24 @@ class Config(metaclass=abc.ABCMeta):
         """
         if isinstance(boolean_value, bool):
             self._write_thru = boolean_value
+        else:
+            raise TypeError("Assignment value to writethru must be a boolean!!")
 
     @property
     def cfgfile(self):
         """
-        Returns the full absolute path of the cfgfile
+        Property
+
+        **cfgfile** - a string 
+
+        Returns the *full absolute path* of the **cfgfile**.
+
+        Can used to set the **cfgfile** name.
+
+        Raises:
+
+            TypeError if **cfgfile** assignment value is not a string.
+
         """
         return(self._cfgfile)
 
@@ -161,8 +259,12 @@ class Config(metaclass=abc.ABCMeta):
     def cfgfile(self, file_name):
         if isinstance(file_name, str):
             self._cfgfile = os.path.abspath(file_name)
+        else:
+            raise TypeError("Assignment value to cfgfile must be a string!!")
 
 
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 if __name__ == "__main__":
 
     from unittest import main
